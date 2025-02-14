7b6ccb6d3e7cfd158ce4f3782a6e42916988298a
Rooholla-KhorrmaBakht <learn.kho@gmail.com>
2025-02-14 14:09:01 -0500

    minor fixes

--- run_simulator.py
+++
@@ -1,6 +0,0 @@
-from NYUFinger.sim import NYUFingerSimulator
-
-# Initialize the simulator with rendering enabled
-simulator = NYUFingerSimulator(render=True)
-
-# Now you can interact with the simulator object in this script
