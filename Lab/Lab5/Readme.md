eb593a43a024cc067b7e0e13f4f5ce9aad0f96a2
Rooholla-KhorramBakht <learn.kho@gmail.com>
2025-04-14 10:23:46 -0400

    lab5 is uploaded.

---
+++ Lab/Lab5/Readme.md
@@ -0,0 +1,14 @@
+# Laboratory 5: Grasping an object
+
+Welcome to the fifth (and last) laboratory of the semester. 
+
+## Goals
+The goal of the laboratory is to implement a graping controller using two fingers
+and using an impedance controller.
+
+
+## Pre-requisites
+Most of the lab can be done on your personal computer (except the real-robot part). You need to have the required packages installed on your computer as instructed for Lab1.
+
+## Instructions
+Complete the notebook
--- NYUFinger/sim.py
+++ NYUFinger/sim.py
@@ -114,3 +114,120 @@ class NYUFingerSimulator:
     def add_ball(self, ball_pos, color=np.array([0, 1, 0, 1]), radius=0.01):
         pos = np.array([ball_pos[0]-0.3-0.06, ball_pos[2], ball_pos[1]+0.29])
         add_visual_capsule(self.scene, pos, pos+np.array([0., 0., 0.001]), radius, color)
+
+
+
+class NYUDualFingerSimulator:
+    def __init__(self, 
+                 render=True, 
+                 dt=0.001, 
+                 xml_path=None,
+                 ):
+
+        if xml_path is None:
+            self.model = mujoco.MjModel.from_xml_path(
+                os.path.join(ASSETS_PATH, 'dual_finger_scene.xml')
+            )
+        else:
+            self.model = mujoco.MjModel.from_xml_path(xml_path)
+
+        self.simulated = True
+        self.data = mujoco.MjData(self.model)
+        self.dt = dt
+        _render_dt = 1 / 60
+        self.render_ds_ratio = max(1, _render_dt // dt)
+
+        if render:
+            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+            self.render = True
+            self.viewer.cam.distance = 3.0
+            self.viewer.cam.azimuth = 90
+            self.viewer.cam.elevation = -45
+            self.viewer.cam.lookat[:] = np.array([0.0, -0.25, 0.824])
+        else:
+            self.render = False
+
+        self.model.opt.gravity[2] = -9.81
+        self.model.opt.timestep = dt
+        self.renderer = None
+        self.render = render
+        self.step_counter = 0
+
+        self.q0 = np.zeros(6)
+        # self.reset()
+        mujoco.mj_step(self.model, self.data)
+        if self.render:
+            self.viewer.sync()
+        self.nv = self.model.nv
+        self.tau_ff = np.zeros(6)
+        self.latest_command_stamp = time.time()
+        self.reset()
+        self.scene = self.viewer.user_scn
+        self.running = True
+        self.sim_thread = threading.Thread(target=self.sim_loop)
+        self.sim_thread.start()
+
+
+    def sim_loop(self):
+        while self.running:
+            # Reset the torque commands if the last command sent by the user is too old (older than 0.1 seconds)
+            if time.time()-self.latest_command_stamp > 0.1: 
+                self.tau_ff[:] = 0.
+            self.step()
+            time.sleep(self.dt)
+
+    def reset(self, q0=None):
+        if q0 is not None:
+            q0 = np.array(q0)
+            assert q0.shape==(6,), 'Wrong q0 shape! The shape should be (6,)'
+            self.data.qpos[:]=q0
+        else:
+            self.data.qpos[2] = 0.0
+            self.data.qpos[5] = 0.0
+        self.data.qpos[6:] = np.array([0.125, 0. , 0.025 , 1, 0, 0, 0, 
+                                       0.125, 0. , 0.075 , 1, 0, 0, 0,
+                                       0.125, 0. , 0.125 , 1, 0, 0, 0])
+        
+        mujoco.mj_step(self.model, self.data)
+        if self.render:
+            self.viewer.sync()
+
+    def get_state(self):
+        return self.data.qpos[:6], self.data.qvel[:6]
+    
+    def get_cube_positions(self):
+        state = dict(
+            cube1=self.data.qpos[6:9],
+            cube2=self.data.qpos[13:16],
+            cube3=self.data.qpos[20:23],
+        )
+        return state
+
+    def send_joint_torque(self, torque):
+        assert np.array(torque).shape == (6,), 'Wrong torque shape! The torque commnand should be a numpy array with shape (6,)'
+        self.latest_command_stamp = time.time()
+        self.tau_ff[:] = np.array(torque)
+
+    def step(self):
+        self.data.ctrl[:6] = self.tau_ff - self.data.qvel[:6]*0.003
+        self.step_counter += 1
+        mujoco.mj_step(self.model, self.data)
+        # Render every render_ds_ratio steps (60Hz GUI update)
+        if self.render and (self.step_counter % self.render_ds_ratio) == 0:
+            self.viewer.sync()
+
+    def close(self):
+        if self.render:
+            self.viewer.close()
+        self.running = False
+        self.sim_thread.join()
+
+    def add_ball(self, ball_pos, color=np.array([0, 1, 0, 1]), radius=0.01):
+        pos = np.array([ball_pos[0]-0.3-0.06, ball_pos[2], ball_pos[1]+0.29])
+        add_visual_capsule(self.scene, pos, pos+np.array([0., 0., 0.001]), radius, color)
+
+    def getFinger1Pos(self):
+        raise NotImplemented('API not implemented.')
+        
+    def getFinger2Pos(self):
+        raise NotImplemented('API not implemented.')
